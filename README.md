d41fb9fd206e2f4d393a43ad79b80ab29676b126
cdaly333 <dalyco884@gmail.com>
2021-09-30 15:07:10 -0400

    add image width arg, fix formatting, use actual python api

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 - `pip3 install sacad`
 - Fill `input.txt` with albums in the format `<Artist>-<Album>`, one per line
-- Run `python3 download_art.py`
+- Run `./download_art.py <Image Width in Pixels>`
--- download_art.py
+++ download_art.py
@@ -1,15 +1,33 @@
-import os
-import re
+#!/usr/bin/env python3
 
-def download_album_art(artist, album):
-    fname = re.sub(r'\W+', '', artist+album)
-    os.system(f'sacad "{artist}" "{album}" 1500 {fname}.png')
+import re
+from sacad import search_and_download, SUPPORTED_IMG_FORMATS
+import asyncio
+from sys import argv
+
+image_width = 500
+if len(argv) > 1 and argv[1].isnumeric():
+  image_width = int(argv[1])
+
+
+async def download_album_art(artist, album):
+  fname = re.sub(r'\W+', '', artist+album)
+  await search_and_download(album, artist, SUPPORTED_IMG_FORMATS["png"], image_width,
+                            f'{fname}.png', size_tolerance_prct=25, amazon_tlds=(), no_lq_sources=False, preserve_format=False)
 
 data = []
 with open("input.txt", "r") as f:
-    for line in f.readlines():
-        data.append(line.split("-"))
+  for line in f.readlines():
+    data.append(line.split("-"))
 
+
+all_tasks = []
 for d in data:
-    print(f"Downloading {d}")
-    download_album_art(d[0], d[1][:-1])
+  print(f"Downloading {d}")
+  all_tasks.append(download_album_art(d[0], d[1][:-1]))
+
+
+async def await_tasks():
+  await asyncio.gather(*all_tasks)
+
+asyncio.run(await_tasks())
